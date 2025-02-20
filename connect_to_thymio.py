eebfa8c2db0fe6ec5e82e1f9220265e302ce8d35
robat2.0 <gabriele.baroli@uni-konstanz.de>
2025-02-20 14:09:53 +0100

    print thymio variables

--- connect_to_thymio.py
+++ connect_to_thymio.py
@@ -1,22 +1,15 @@
 from thymiodirect import Connection, Thymio
+import time
+import numpy as np
+
 try:
     port = Connection.serial_default_port()
     th = Thymio(serial_port=port, 
                 on_connect=lambda node_id: print(f'Thymio {node_id} is connected'))
     th.connect()
     robot_id = th.first_node()
-    # print(th.variables(robot_id))
+    print(th.variables(robot_id))
     robot = th[robot_id]
-    try:
-        # robot['motor.left.target'] = 200
-        # robot['motor.right.target'] = 200
-        while True:
-            print(robot['prox.ground.ambiant'], robot['prox.ground.reflected'], robot['prox.ground.delta'])
-
-    except KeyboardInterrupt:
-        robot['motor.left.target'] = 0
-        robot['motor.right.target'] = 0
-        print('Basta')
     try:
         th.disconnect()
     except Exception as e:

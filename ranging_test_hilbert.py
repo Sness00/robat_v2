88c85ac6177ea67b26624ec21b5c77ee0d933756
robat2.0 <gabriele.baroli@uni-konstanz.de>
2025-03-07 15:31:43 +0100

    peaks  threshold fine tuning

--- ranging_test_hilbert.py
+++ ranging_test_hilbert.py
@@ -88,70 +88,48 @@ if __name__ == "__main__":
     while not audio_in_data.empty():
         all_input_audio.append(audio_in_data.get())            
     input_audio = np.concatenate(all_input_audio)
-
-    # echoes = input_audio[:2650, :]
-
-    # rms = 20*np.log10(np.std(echoes, axis=0))
-
-    # print(rms)
-
-
-    valid_channels_audio = input_audio
-    filtered_signals = signal.correlate(valid_channels_audio, np.reshape(sig, (-1, 1)), 'same', method='fft')
-    roll_filt_sigs = np.roll(filtered_signals, -len(sig)//2, axis=0)
-    envelopes = np.abs(signal.hilbert(roll_filt_sigs, axis=0))
-    mean_envelope = np.sum(envelopes, axis=1)/envelopes.shape[1]
-
-    idxs, _ = signal.find_peaks(mean_envelope, prominence=10)
-    emission_peak = idxs[0]    
-
-    peaks = []
-    enough = True
-    for i in np.arange(envelopes.shape[1]):
-        idxs, _ = signal.find_peaks(envelopes[emission_peak + discarded_samples:, i], prominence=2)
-        if idxs.any():
-            peaks.append(idxs[0] + emission_peak + discarded_samples)
-        else:
-            enough = False
-            
-    if not enough:
-        print('No peaks detected')
-    else:
-        estimated_distances = []
-        mean_dist = 0
-        for i, p in enumerate(peaks):
-            dist = (p - emission_peak)/fs*C_AIR/2 + 0.025
-            estimated_distances.append(dist)
-            print('Estimated distance for channel', i+1, ':', '%.3f' % dist, '[m]')    
-            mean_dist += dist
-        mean_dist /= len(peaks)
-        peaks_array = np.array(peaks)
-
-        print('Estimated mean distance: %.3f' % mean_dist, '[m]')
-
-    t_plot = np.linspace(0, envelopes.shape[0]/fs, envelopes.shape[0])
-    fig, ax = plt.subplots(4, 2, sharex=True, sharey=True)
-    plt.suptitle('Channel Envelopes')
-    for i in range(envelopes.shape[1]//2):
-        for j in range(2):
-            ax[i, j].plot(t_plot, envelopes[:, 2*i+j])
-            if enough:
-                ax[i, j].axvline(emission_peak/fs, 0, 1.1*max(mean_envelope[:emission_peak]), linestyle='dashed', color='g')
-                ax[i, j].axvline(peaks_array[2*i+j]/fs, 0, 1.1*max(envelopes[emission_peak:, 2*i+j]), linestyle='dashed', color='r')
-            ax[i, j].set_title('Channel %d' % (2*i+j+1))
-            ax[i, j].minorticks_on()
-            ax[i, j].grid(which='minor', linestyle=':', linewidth='0.5', color='gray')
-            ax[i, j].grid()
-    plt.tight_layout()
-    plt.show()
     
-    if verbose: 
-        t_plot1 = np.linspace(0, input_audio.shape[0]/fs, input_audio.shape[0])
+    if (20*np.log10(np.mean(np.std(input_audio, axis=0)))) > -55:
+
+        valid_channels_audio = input_audio
+        filtered_signals = signal.correlate(valid_channels_audio, np.reshape(sig, (-1, 1)), 'same', method='fft')
+        roll_filt_sigs = np.roll(filtered_signals, -len(sig)//2, axis=0)
+        envelopes = np.abs(signal.hilbert(roll_filt_sigs, axis=0))
+        mean_envelope = np.sum(envelopes, axis=1)/envelopes.shape[1]
+
+        idxs, _ = signal.find_peaks(mean_envelope, prominence=10)
+        emission_peak = idxs[0]    
+
+        peaks = []
+        enough = True
+        for i in np.arange(envelopes.shape[1]):
+            idxs, _ = signal.find_peaks(envelopes[emission_peak + discarded_samples:, i], prominence=2)
+            if idxs.any():
+                peaks.append(idxs[0] + emission_peak + discarded_samples)
+            else:
+                enough = False
+                
+        if not enough:
+            print('No peaks detected')
+        else:
+            estimated_distances = []
+            mean_dist = 0
+            for i, p in enumerate(peaks):
+                dist = (p - emission_peak)/fs*C_AIR/2 + 0.025
+                estimated_distances.append(dist)
+                print('Estimated distance for channel', i+1, ':', '%.3f' % dist, '[m]')    
+                mean_dist += dist
+            mean_dist /= len(peaks)
+            peaks_array = np.array(peaks)
+
+            print('Estimated mean distance: %.3f' % mean_dist, '[m]')
+
+        t_plot = np.linspace(0, envelopes.shape[0]/fs, envelopes.shape[0])
         fig, ax = plt.subplots(4, 2, sharex=True, sharey=True)
-        plt.suptitle('Recorded Audio')
-        for i in range(input_audio.shape[1]//2):
+        plt.suptitle('Channel Envelopes')
+        for i in range(envelopes.shape[1]//2):
             for j in range(2):
-                ax[i, j].plot(t_plot1, input_audio[:, 2*i+j])
+                ax[i, j].plot(t_plot, envelopes[:, 2*i+j])
                 if enough:
                     ax[i, j].axvline(emission_peak/fs, 0, 1.1*max(mean_envelope[:emission_peak]), linestyle='dashed', color='g')
                     ax[i, j].axvline(peaks_array[2*i+j]/fs, 0, 1.1*max(envelopes[emission_peak:, 2*i+j]), linestyle='dashed', color='r')
@@ -161,19 +139,38 @@ if __name__ == "__main__":
                 ax[i, j].grid()
         plt.tight_layout()
         plt.show()
+        
+        if verbose: 
+            t_plot1 = np.linspace(0, input_audio.shape[0]/fs, input_audio.shape[0])
+            fig, ax = plt.subplots(4, 2, sharex=True, sharey=True)
+            plt.suptitle('Recorded Audio')
+            for i in range(input_audio.shape[1]//2):
+                for j in range(2):
+                    ax[i, j].plot(t_plot1, input_audio[:, 2*i+j])
+                    if enough:
+                        ax[i, j].axvline(emission_peak/fs, 0, 1.1*max(mean_envelope[:emission_peak]), linestyle='dashed', color='g')
+                        ax[i, j].axvline(peaks_array[2*i+j]/fs, 0, 1.1*max(envelopes[emission_peak:, 2*i+j]), linestyle='dashed', color='r')
+                    ax[i, j].set_title('Channel %d' % (2*i+j+1))
+                    ax[i, j].minorticks_on()
+                    ax[i, j].grid(which='minor', linestyle=':', linewidth='0.5', color='gray')
+                    ax[i, j].grid()
+            plt.tight_layout()
+            plt.show()
 
-        t_plot2 = np.linspace(0, roll_filt_sigs.shape[0]/fs, roll_filt_sigs.shape[0])
-        fig2, ax2 = plt.subplots(4, 2, sharex=True, sharey=True)
-        plt.suptitle('Matched Filter Output')
-        for i in range(roll_filt_sigs.shape[1]//2):
-            for j in range(2):
-                ax2[i, j].plot(t_plot2, roll_filt_sigs[:, 2*i+j])
-                if enough:
-                    ax2[i, j].axvline(emission_peak/fs, 0, 1.1*max(mean_envelope[:emission_peak]), linestyle='dashed', color='g')
-                    ax2[i, j].axvline(peaks_array[2*i+j]/fs, 0, 1.1*max(envelopes[emission_peak:, 2*i+j]), linestyle='dashed', color='r')
-                ax2[i, j].set_title('Channel %d' % (2*i+j+1))
-                ax2[i, j].minorticks_on()
-                ax2[i, j].grid(which='minor', linestyle=':', linewidth='0.5', color='gray')
-                ax2[i, j].grid()
-        plt.tight_layout()
-        plt.show() 
+            t_plot2 = np.linspace(0, roll_filt_sigs.shape[0]/fs, roll_filt_sigs.shape[0])
+            fig2, ax2 = plt.subplots(4, 2, sharex=True, sharey=True)
+            plt.suptitle('Matched Filter Output')
+            for i in range(roll_filt_sigs.shape[1]//2):
+                for j in range(2):
+                    ax2[i, j].plot(t_plot2, roll_filt_sigs[:, 2*i+j])
+                    if enough:
+                        ax2[i, j].axvline(emission_peak/fs, 0, 1.1*max(mean_envelope[:emission_peak]), linestyle='dashed', color='g')
+                        ax2[i, j].axvline(peaks_array[2*i+j]/fs, 0, 1.1*max(envelopes[emission_peak:, 2*i+j]), linestyle='dashed', color='r')
+                    ax2[i, j].set_title('Channel %d' % (2*i+j+1))
+                    ax2[i, j].minorticks_on()
+                    ax2[i, j].grid(which='minor', linestyle=':', linewidth='0.5', color='gray')
+                    ax2[i, j].grid()
+            plt.tight_layout()
+            plt.show()
+    else:
+        print('Low input level. Dead battery?')

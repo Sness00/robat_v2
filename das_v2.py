cf6accb18444d0b25ee0bb66d005e39f8a00f439
robat2.0 <gabriele.baroli@uni-konstanz.de>
2025-03-20 18:46:26 +0100

    normalized das in each band seems better

--- das_v2.py
+++ das_v2.py
@@ -35,27 +35,28 @@ def das_filter(y, fs, nch, d, bw, theta=np.linspace(-90, 90, 73), c=343, wlen=64
   f_spec_axis, _, spectrum = stft(y, fs=fs, window=np.ones((wlen, )), nperseg=wlen, noverlap=wlen-1, axis=0)
   bands = f_spec_axis[(f_spec_axis >= bw[0] + 7500) & (f_spec_axis <= bw[1] - 7500)]
   p = np.zeros_like(theta, dtype=complex)
-  p_i = np.zeros((len(theta), 1), dtype=complex)
+  p_i = np.zeros_like(theta, dtype=complex)
 
-  for f_c in bands:
+  for j, f_c in enumerate(bands):
       w_s = (2*np.pi*f_c*d*np.sin(np.deg2rad(theta))/c)        
       a = np.exp(np.outer(np.linspace(0, nch-1, nch), -1j*w_s))
       a_H = a.T.conj()     
       spec = spectrum[f_spec_axis == f_c, :, :].squeeze()
       cov_est = np.cov(spec, bias=True)
       
-      for i, _ in enumerate(theta):
-        
+      for i in range(len(theta)):        
         p_i[i] = a_H[i, :] @ cov_est @ a[:, i]/(nch**2)
-        p[i] += p_i[i]
+      
+      p += np.abs(p_i)/np.max(np.abs(p_i))
+      
       if show:
         plt.figure()
-        plt.polar(np.deg2rad(theta), 20*np.log10(np.abs(p_i)))
+        plt.polar(np.deg2rad(theta), 20*np.log10(np.abs(p_i)/np.max(np.abs(p_i))))
         plt.xlim((-np.pi/2, np.pi/2))
-        plt.ylim((-90, 30))
+        plt.ylim((-30, 1))
         plt.title(str(f_c))
         plt.show()
   
   mag_p = np.abs(p)/len(bands)
-      
+  
   return theta, mag_p

6250daaadbc84fad6c3c387b817e4ffc026bb80d
robat2.0 <gabriele.baroli@uni-konstanz.de>
2025-03-31 14:52:51 +0200

    keep it like that

--- connect_to_thymio.py
+++ connect_to_thymio.py
@@ -1,6 +1,5 @@
 from thymiodirect import Connection, Thymio
 import time
-import numpy as np
 
 try:
     port = Connection.serial_default_port()
@@ -9,25 +8,7 @@ try:
     th.connect()
     robot_id = th.first_node()
     print(th.variables(robot_id))
-    robot = th[robot_id]
-    robot['motor.left.target'] = 100
-    robot['motor.right.target'] = 100
-    try:
-        while True:
-            print('#'*80)
-            print(robot['prox.ground.reflected'])
-            print(robot['prox.ground.ambiant'])
-            print(robot['prox.ground.delta'])
-            print('#'*80)
-            time.sleep(0.1)
-    except KeyboardInterrupt:
-         
-        try:
-            robot['motor.left.target'] = 0
-            robot['motor.right.target'] = 0
-            time.sleep(1)
-            th.disconnect()
-        except Exception as e:
-                print(e)
+    time.sleep(1)
+    th.disconnect()
 except Exception as e:
     print(e)
